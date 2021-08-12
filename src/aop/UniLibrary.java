b87df84920451be079c809eb55ce4d44e08741d3
Dmitrii Trofimov <probcf@gmail.com>
2021-08-12 20:29:27 +0300

    AOP Advice type @Around with Exception

--- src/aop/UniLibrary.java
+++ src/aop/UniLibrary.java
@@ -22,6 +22,7 @@ public class UniLibrary extends AbstractLibrary {
 	}
 
 	public String returnBook() {
+		int a = 10 / 0;
 		System.out.println("Мы возвращаем книгу в UniLibrary.");
 		System.out.println("-----------------------------------------------");
 		return "Война и мир.";
